32ca4b41d94e2e12b504bda7ab48909f3f42030a
Harsh Devisha <harshdevisha9111@gmail.com>
2026-01-11 22:47:59 -0800

    cleanup: removed uncommitted-log tests

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 const log = @import("log/log.zig");
 const protocol = @import("network/protocol.zig");
-const uncommitted = @import("replication/uncommitted.zig");
 const follower_tracker = @import("replication/follower_tracker.zig");
 const follower = @import("replication/follower.zig");
 const leader = @import("replication/leader.zig");
@@ -11,7 +10,6 @@ const event_loop = @import("event_loop.zig");
 test {
     std.testing.refAllDecls(log);
     std.testing.refAllDecls(protocol);
-    std.testing.refAllDecls(uncommitted);
     std.testing.refAllDecls(follower_tracker);
     std.testing.refAllDecls(follower);
     std.testing.refAllDecls(leader);
